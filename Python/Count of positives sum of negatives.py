49f1d843a39dbfbcf83add477a4c295a15269f1f
Stephen Cripps <stephen.cripps@tutanota.com>
2022-01-18 21:05:56 +0000

    Create Count of positives sum of negatives.py

---
+++ Python/Count of positives sum of negatives.py
@@ -0,0 +1,19 @@
+import test
+
+
+def count_positives_sum_negatives(arr):
+    if(len(arr) == 0):
+        return []
+    return [len(list(filter(lambda value: value > 0, arr))),
+            sum(list(filter(lambda value: value < 0, arr)))]
+
+
+test.assert_equals(count_positives_sum_negatives(
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]), [10, -65])
+test.assert_equals(count_positives_sum_negatives(
+    [0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14]), [8, -50])
+test.assert_equals(count_positives_sum_negatives([1]), [1, 0])
+test.assert_equals(count_positives_sum_negatives([-1]), [0, -1])
+test.assert_equals(count_positives_sum_negatives(
+    [0, 0, 0, 0, 0, 0, 0, 0, 0]), [0, 0])
+test.assert_equals(count_positives_sum_negatives([]), [])
