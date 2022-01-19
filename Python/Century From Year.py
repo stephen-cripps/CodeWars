b987706e31b87539f0e802e620ed170f7cb39568
Stephen Cripps <stephen.cripps@tutanota.com>
2022-01-19 22:05:20 +0000

    Create Century From Year.py

---
+++ Python/Century From Year.py
@@ -0,0 +1,13 @@
+import test
+
+
+def century(year):
+    return (year - 1)//100 + 1
+
+
+test.assert_equals(century(1705), 18)
+test.assert_equals(century(1900), 19)
+test.assert_equals(century(1601), 17)
+test.assert_equals(century(2000), 20)
+test.assert_equals(century(356), 4)
+test.assert_equals(century(89), 1)
